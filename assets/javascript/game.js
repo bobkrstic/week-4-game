972bf84d33bf862278afe6a49fbe5ddd5c214f1d
Bob Krstic <bobkrstic@Bobs-iMac.local>
2017-05-13 21:38:10 -0700

    Initial final code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,3 +12,140 @@
 // player score variable
 
 // compare if the playerScore is eqal or greater to holdsRandomNumber
+
+// if the playerScore is equal to holdsRandomNumber wins++
+// if the playerScore is greater then holdsRandomNumber losses++
+
+// reset all variables
+// reset playerScore
+// reset holdsRandomNumber
+// start game from the begining keeping wins and losses result active
+
+$(document).ready(function () {
+
+	var red = 0;
+	var blue = 0;
+	var yellow = 0;
+	var green = 0;
+	var playerScoreNumber = 0;
+	var numberToGuess = 0;
+	var win = 0;
+	var lose = 0; 
+
+    startGame();
+
+	// var number = 1 + Math.floor(Math.random() * 12);
+function startGame() {
+
+	red = 1 + Math.floor(Math.random() * 12);
+	blue = 1 + Math.floor(Math.random() * 12);
+	yellow = 1 + Math.floor(Math.random() * 12);
+	green = 1 + Math.floor(Math.random() * 12);
+	playerScoreNumber = 0;
+	
+
+	console.log(red, blue, yellow, green);
+
+	numberToGuess = Math.floor(Math.random() * 102) + 19;
+	console.log(numberToGuess);
+	$("#holdsRandomNumber").text(numberToGuess);
+	$("#playerScore").text(playerScoreNumber);
+	$("#winsScore").text(win);
+	$("#lossesScore").text(lose);
+
+}
+
+		$("#redDiamond").on("click", function() {
+			// alert("working");
+			playerScoreNumber = playerScoreNumber + red;
+			$("#playerScore").text(playerScoreNumber);
+
+	 			if (playerScoreNumber === numberToGuess) {
+			      alert("You win!");
+			      win++;
+			      startGame();
+			    }
+
+			    else if (playerScoreNumber >= numberToGuess) {
+			      alert("You lose!!");
+			      lose++;
+			      startGame();
+			    }
+
+
+		});
+
+
+
+
+		$("#blueDiamond").on("click", function() {
+			// alert("working");
+			playerScoreNumber = playerScoreNumber + blue;
+			$("#playerScore").text(playerScoreNumber);
+
+			 			if (playerScoreNumber === numberToGuess) {
+					      alert("You win!");
+					      win++;
+					      startGame();
+					    }
+
+					    else if (playerScoreNumber >= numberToGuess) {
+					      alert("You lose!!");
+					      lose++;
+					      startGame();
+					    }
+
+		});
+
+
+
+
+
+		$("#yellowDiamond").on("click", function() {
+			// alert("working");
+			playerScoreNumber = playerScoreNumber + yellow;
+			$("#playerScore").text(playerScoreNumber);
+		 			if (playerScoreNumber === numberToGuess) {
+				      alert("You win!");
+				      win++;
+				      startGame();
+				    }
+
+				    else if (playerScoreNumber >= numberToGuess) {
+				      alert("You lose!!");
+				      lose++;
+				      startGame();
+				    }
+
+		});
+
+
+
+
+
+		$("#greenDiamond").on("click", function() {
+			// alert("working");
+			playerScoreNumber = playerScoreNumber + green;
+			$("#playerScore").text(playerScoreNumber);
+
+ 			if (playerScoreNumber === numberToGuess) {
+		      alert("You win!");
+		      win++;
+		      startGame();
+		    }
+
+		    else if (playerScoreNumber >= numberToGuess) {
+		      alert("You lose!!");
+		      lose++;
+		      startGame();
+		    }
+		});
+
+
+
+
+
+
+
+
+});
