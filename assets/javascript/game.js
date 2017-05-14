44a9a8f89dbe6a71ac15a19da1f9de59d462b136
Bob Krstic <bobkrstic@Bobs-iMac.local>
2017-05-13 19:04:21 -0700

    initial coding html

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// every time game starts set random values for each diamond between
+// 1 and 12
+
+// set holdsRandomNumber variable
+// set playerScore var
+
+// set random number for the user to see
+// this random number will be the number to guess/compare
+// with the result that the user is going to get pressing diamonds
+
+// each time diamond is pressed add that value to the
+// player score variable
+
+// compare if the playerScore is eqal or greater to holdsRandomNumber
